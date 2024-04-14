ac918f1ed2c1a41f2c0c18e77c2dd7e25d11a35d
zaklance <wosewick@gmail.com>
2024-04-14 08:45:46 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+      });
 }
